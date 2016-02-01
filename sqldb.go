fbf53998fa34ae4fc111c7e4832094c4d79a730c
shaojw <shaojw@autelan.com>
2016-02-01 18:16:28 +0800

    debug read repository

--- sqldb.go
+++ sqldb.go
@@ -19,6 +19,7 @@ package eventhorizon
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"time"
 
 	"database/sql/driver"
@@ -93,13 +94,7 @@ func (u *UUID) Scan(value interface{}) error {
 }
 
 // NewSqlEventStore creates a new SqlEventStore.
-func NewSqlEventStore(eventBus EventBus, driver, url string) (*SqlEventStore, error) {
-	db, err := gorm.Open(driver, url)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	db.LogMode(true)
+func NewSqlEventStore(eventBus EventBus, db gorm.DB) (*SqlEventStore, error) {
 	db.AutoMigrate(&sqlAggregateRecord{})
 	db.AutoMigrate(&sqlEventRecord{})
 
@@ -165,6 +160,10 @@ func (s *SqlEventStore) Save(events []Event) error {
 func (s *SqlEventStore) Load(id UUID) ([]Event, error) {
 	var eventRecords []sqlEventRecord
 	s.db.Where("ID = ?", id.String()).Find(&eventRecords)
+	fmt.Println("eventRecords.type", reflect.TypeOf(eventRecords), reflect.TypeOf(eventRecords).Size())
+	v := reflect.ValueOf(eventRecords)
+	fmt.Println("eventRecords v.Type", v.Type(), v.Type().Size())
+	fmt.Println("eventRecords", eventRecords)
 
 	events := make([]Event, len(eventRecords))
 	for i, eventRecord := range eventRecords {
@@ -198,6 +197,139 @@ func (s *SqlEventStore) RegisterEventType(event Event, factory func() Event) err
 	}
 
 	s.factories[event.EventType()] = factory
+
+	return nil
+}
+
+// MongoReadRepository implements an MongoDB repository of read models.
+type SqlReadRepository struct {
+	db      gorm.DB
+	factory func() interface{}
+}
+
+type SqlBaseEntry struct {
+	ID        UUID       `gorm:"primary_key"`
+	CreatedAt time.Time  `sql:"index"`
+	UpdatedAt time.Time  `sql:"index"`
+	DeletedAt *time.Time `sql:"index"`
+}
+
+// NewMongoReadRepository creates a new MongoReadRepository.
+func NewSqlReadRepository(db gorm.DB, factory func() interface{}) (*SqlReadRepository, error) {
+	r := &SqlReadRepository{
+		db:      db,
+		factory: factory,
+	}
+
+	model := factory()
+
+	db.AutoMigrate(model)
+
+	return r, nil
+}
+
+// Save saves a read model with id to the repository.
+func (r *SqlReadRepository) Save(id UUID, model interface{}) error {
+	m := r.factory()
+
+	var c uint
+	r.db.Model(m).Where("id =?", id).Count(&c)
+	if c > 0 {
+		r.db.Save(model)
+	} else {
+		r.db.Create(model)
+	}
+	return nil
+}
+
+// Find returns one read model with using an id. Returns
+// ErrModelNotFound if no model could be found.
+func (r *SqlReadRepository) Find(id UUID) (interface{}, error) {
+	model := r.factory()
+
+	r.db.Where("id =?", id).First(model)
+	return model, nil
+}
+
+// FindCustom uses a callback to specify a custom query.
+func (r *SqlReadRepository) FindPage(query, order string, page, size uint) ([]interface{}, uint, error) {
+
+	m := r.factory()
+
+	begin := page * size
+	var allcount uint
+	if query == "" {
+		r.db.Model(m).Count(&allcount)
+	} else {
+		r.db.Model(m).Where(query).Count(&allcount)
+	}
+
+	if begin > allcount {
+		return []interface{}{}, allcount, nil
+	}
+
+	count := size
+	if allcount-begin < size { //last page
+		count = allcount - begin
+	}
+
+	if order == "" {
+		order = "updated_at"
+	}
+
+	v := reflect.ValueOf(m).Elem()
+	sv := reflect.MakeSlice(reflect.SliceOf(v.Type()), 0, 0)
+
+	snv := reflect.New(sv.Type())
+	result := snv.Interface()
+
+	if query != "" {
+		r.db.Where(query).Order(order).Offset(begin).Limit(count).Find(result)
+	} else {
+		r.db.Order(order).Offset(begin).Limit(count).Find(result)
+	}
+
+	rv := reflect.Indirect(reflect.ValueOf(result))
+
+	ret := make([]interface{}, rv.Len())
+	for i := 0; i < rv.Len(); i++ {
+		ret[i] = rv.Index(i).Interface()
+	}
+
+	return ret, allcount, nil
+}
+
+// FindAll returns all read models in the repository.
+func (r *SqlReadRepository) FindAll() ([]interface{}, error) {
+	m := r.factory()
+
+	var allcount uint
+	r.db.Model(m).Count(&allcount)
+
+	v := reflect.ValueOf(m).Elem()
+	sv := reflect.MakeSlice(reflect.SliceOf(v.Type()), 0, 0)
+
+	snv := reflect.New(sv.Type())
+	result := snv.Interface()
+
+	r.db.Find(result)
+
+	rv := reflect.Indirect(reflect.ValueOf(result))
+
+	ret := make([]interface{}, rv.Len())
+	for i := 0; i < rv.Len(); i++ {
+		ret[i] = rv.Index(i).Interface()
+	}
+
+	return ret, nil
+}
+
+// Remove removes a read model with id from the repository. Returns
+// ErrModelNotFound if no model could be found.
+func (r *SqlReadRepository) Remove(id UUID) error {
+	m := r.factory()
+
+	r.db.Where("id =", id).Delete(m)
 
 	return nil
 }
